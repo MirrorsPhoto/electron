2495fed80ce6129665fd67cc6098c0bcb0128491
klimash <andrey.klimash@gmail.com>
2017-08-25 11:19:17 +0300

    fix validation auth form

--- src/components/pages/auth.vue
+++ src/components/pages/auth.vue
@@ -1,63 +1,77 @@
 <template>
     <div class="bg">
-        <form ref="form" action="" @submit.prevent="auth()">
+        <form :class="{ error }" action="" @submit.prevent="auth()">
             <img src="../../assets/logo.svg"/>
             <field
                 placeholder="Логин"
-                v-model="login"
+                v-model="user.login"
                 autofocus
             ></field>
             <field
                 placeholder="Пароль"
                 type="password"
-                v-model="password"
+                v-model="user.password"
             ></field>
             <input type="submit">
+
+            <p v-if="error">{{ errorText }}</p>
         </form>
     </div>
 </template>
 
 <script>
-    import field from '../UI/field.vue'
+    import field from '../UI/field.vue';
 
     export default {
         data() {
             return {
-                login: '',
-                password: ''
+                user: {
+                    login: '',
+                    password: ''
+                },
+                error: false,
+                errorText: ''
+            }
+        },
+        watch: {
+            user: {
+                handler() {
+                    this.error = false;
+                },
+                deep: true
             }
         },
         computed: {
             isValid() {
-                return !!this.login && !!this.password
+                return !!this.user.login && !!this.user.password;
             }
         },
         methods: {
             auth() {
                 if (this.isValid) {
-                    const { login, password, $router, shakeForm } = this
+                    const { login, password } = this.user;
 
                     this.ajax({
                         url: 'login',
                         method: 'post',
                         data: { login, password },
-                        success(response) {
-                            const token = response.data.response.token
-                            localStorage.setItem('token', token)
-                            $router.push('/')
+                        success: response => {
+                            const token = response.data.response.token;
+                            localStorage.setItem('token', token);
+                            this.$router.push('/');
                         },
-                        error(error) {
-                            shakeForm();
+                        error: error => {
                             const message = JSON.parse(error.request.responseText).message;
-                            alert(message);
+                            this.error = true;
+                            this.errorText = message;
                         }
-                    })
+                    });
 
-                   
                 }
-            },
-            shakeForm() {
-                this.$refs.form.classList.add('error');
+                else {
+                    this.error = true;
+                    this.errorText = 'Заполните все поля формы';
+                }
             }
         },
         components: {
@@ -71,6 +85,8 @@
 </script>
 
 <style lang="sass">
+@import '../../styles_config.sass'
+
 .bg
     background: url(../../assets/bg.jpg) center center no-repeat
     background-size: cover
@@ -81,7 +97,8 @@
 
 form 
     width: 350px
-    padding: 50px
+    padding: 70px 50px
+    position: relative
     color: #fff
     background: #fff
     box-shadow: 1px 5px 10px rgba(0, 0, 0, .2)
@@ -97,203 +114,27 @@ form
     & [type="submit"]
         display: none
 
-    &.error 
-        animation: shake-little 1s ease-in-out
+    & p 
+        text-align: center
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 30px
+        color: $primary-color
+
+    &.error
+      animation: shake 0.2s ease-in-out 0s 2
+    
+
+@keyframes shake 
+  0% 
+    margin-left: 0rem
+  25% 
+    margin-left: 0.5rem
+  75%
+    margin-left: -0.5rem
+  100% 
+    margin-left: 0rem
 
-@keyframes shake-little 
-    2% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-    4% 
-        transform: translate(0px, 0px) rotate(0.5deg)
-
-
-    6% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    8% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-    10% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    12% 
-        transform: translate(0px, 0px) rotate(0.5deg)
-
-
-    14% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    16% 
-        transform: translate(0px, 0px) rotate(0.5deg)
-
-
-    18% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    20% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    22% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    24% 
-        transform: translate(0px, 0px) rotate(0.5deg)
-
-
-    26% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    28% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    30% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    32% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    34% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    36% 
-        transform: translate(0px, 0px) rotate(0.5deg)
-
-
-    38% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    40% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-    42% 
-        transform: translate(0px, 0px) rotate(0.5deg)
-
-
-    44% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    46% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-    48% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    50% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    52% 
-        transform: translate(0px, 0px) rotate(0.5deg)
-
-
-    54% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    56% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    58% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    60% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    62% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    64% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    66% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    68% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    70% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    72% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    74% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    76% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    78% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    80% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    82% 
-        transform: translate(0px, 0px) rotate(0.5deg)
-
-
-    84% 
-        transform: translate(0px, 0px) rotate(0.5deg)
-
-
-    86% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    88% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-
-    90% 
-        transform: translate(0px, 0px) rotate(0.5deg)
-
-
-    92% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    94% 
-        transform: translate(0px, 1px) rotate(0.5deg)
-
-
-    96% 
-        transform: translate(1px, 0px) rotate(0.5deg)
-
-
-    98% 
-        transform: translate(1px, 1px) rotate(0.5deg)
-
-    100% 
-        transform: translate(0, 0) rotate(0)
-  
 
 </style>

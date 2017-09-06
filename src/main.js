80cf951e92e80840aa267264abe4344a3164cea8
jonkofee <jonkofee@icloud.com>
2017-09-06 11:55:23 +0300

    роутинг и axios в отдельный файл

--- src/main.js
+++ src/main.js
@@ -1,47 +1,10 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Axios from 'axios';
 
-import routes from './routes';
-
+import router from './router';
 import App from './App';
+import axios from './axios';
 
-
-//  Vue-Router
-Vue.use(VueRouter);
-const router = new VueRouter({ routes });
-router.beforeEach((to, from, next) => {
-    if (to.matched.some(record => record.meta.private)) {
-        const token = localStorage.getItem('token');
-        (!token) ? next({ path: '/login' }) : next();
-    }
-    else next();
-});
-
-//  Axios
-Vue.prototype.$http = Axios.create({
-    baseURL: 'http://api.jonkofee.ru'
-});
-
-Vue.prototype.$http.interceptors.request.use(
-    response => {
-        const token = localStorage.getItem('token');
-        if (token) response.headers['Authorization'] = 'Bearer ' + token;
-        return response;
-    },
-    error => Promise.reject(error)
-);
-
-Vue.prototype.$http.interceptors.response.use(
-    response => response,
-    error => {
-        if (error.request.status === 401) {
-            if (localStorage.getItem('token')) localStorage.removeItem('token');
-            vm.$router.push('/login');
-        }
-        return Promise.reject(error);
-    }
-);
+Vue.prototype.$http = axios;
 
 const vm = new Vue({
     el: '#app',

2a8b9862911d5ab7087babefa5ad0fba8dedaaf9
Unknown <andrey.klimash@gmail.com>
2017-10-02 13:17:03 +0300

    fix logo.vue

--- src/components/UI/logo.vue
+++ src/components/UI/logo.vue
@@ -1,12 +1,19 @@
 <template>
     <svg width="1800" height="1000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1800 1000">
         <g class="bg" transform="skewY(-5)">
-            <polygon points="0 157, 1800 157, 1800 1000, 0 1000" :fill-opacity=" percent ? '0.7' : '1'"/>
-            <polygon points="0 157, 1800 157, 1800 1000, 0 1000" class="progress-bar" :style="'transition: transform .5s ease-in-out; transform: scaleX(' + percent / 100 + ')'"/>
+            <polygon
+                points="0 157, 1800 157, 1800 1000, 0 1000"
+                :fill-opacity="(percent > 0) ? '0.7' : '1'"
+            />
+            <polygon
+                class="progress-bar"
+                points="0 157, 1800 157, 1800 1000, 0 1000"
+                :style="'transform: scaleX(' + percent / 100 + ')'"
+            />
         </g>
         <g class="label">
-            <text x="150" y="500" fill="#FFF" font-family="Mirrors" font-size="190">Mirror's</text>
-            <text x="150" y="650" fill="#FFF" font-family="Mirrors" font-size="120">Photo</text>
+            <text x="150" y="500" font-size="190">Mirror's</text>
+            <text x="150" y="650" font-size="120">Photo</text>
         </g>
     </svg>
 </template>
@@ -15,16 +22,19 @@
     export default {
         data() {
             return {
-                percent: 0,
+                percent: 0
+            }
+        },
+        methods: {
+            setPercent(data, listener) {
+                this.percent = listener.percent
             }
         },
         mounted() {
-            this.$electron.ipcRenderer.on('au-download-progress', (data, listener) => {
-                this.percent = listener.percent;
-            })
+            this.$electron.ipcRenderer.on('au-download-progress', this.setPercent)
         },
-        destroyed(){
-            this.$electron.ipcRenderer.removeListener('au-download-progress', () => {});
+        destroyed() {
+            this.$electron.ipcRenderer.removeListener('au-download-progress', this.setPercent)
         }
     }
 </script>
@@ -40,6 +50,10 @@
         fill: $primary-color
 
     .label text
-        font-family: Mirrors !important
+        font-family: Mirrors
+        fill: #fff
+
+    .progress-bar
+        transition: transform .5s ease-in-out;
 
 </style>

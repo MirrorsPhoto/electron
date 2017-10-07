7e8a48bfbba1108fddebc2d899545669635dc12b
Andrey Klimash <andrey.klimash@gmail.com>
2017-10-08 01:45:44 +0300

    total summ in check

--- src/components/modules/check.vue
+++ src/components/modules/check.vue
@@ -1,22 +1,35 @@
 <template>
     <div class="widget_wrap">
         <table>
+            <th>
+                <td>№</td>
+                <td>Наименование</td>
+                <td>Кол-во</td>
+                <td>Цена</td>
+            </th>
             <tr v-for="(row, index) in rows" :key="index">
                 <td>{{ index + 1 }}</td>
-                <td>{{ row.name }}</td>
-                <td>{{ row.value }}</td>
+                <td>{{ row.name }}, {{ row.value }}</td>
                 <td>{{ row.count }}</td>
+                <td>{{ row.price }}</td>
             </tr>
+            <tfoot>
+                <td>{{ total }}</td>
+                <td><button @click.prevent="confirm()">Оплачено</button></td>
+            </tfoot>
         </table>
-        <button @click.prevent="confirm()">Оплачено</button>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            rows: [],
-            summ: 20
+            rows: []
+        }
+    },
+    computed:{
+        total() {
+            return this.rows.reduce((res, row) => res += row.price, 0)
         }
     },
     methods: {
@@ -24,7 +37,7 @@ export default {
             this.rows.push(data)
         },
         confirm() {
-            if (this.summ > 0) this.$store.commit('addSale', this.summ)
+            if (this.total > 0) this.$store.commit('addSale', this.total)
         }
     },
     mounted() {

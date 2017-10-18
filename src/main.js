ca0d7313a005900743bea2a2f4992744cebd4756
Unknown <andrey.klimash@gmail.com>
2017-10-18 11:34:22 +0300

    Починил обработку 401 ошибки

--- src/main.js
+++ src/main.js
@@ -4,16 +4,18 @@ import Axios from './plugins/axios'
 
 import store from './store'
 
-
 Vue.use(VueElectron)
-Vue.prototype.$http = Axios
 Vue.config.productionTip = false
 Vue.config.performance = true
 
-
-const vm = new Vue({
-    el: '#app',
-    store,
-    template: '<App ref="app"></App>',
-    components: { App: require('./App') }
+new Vue({
+  el: '#app',
+  store,
+  template: '<App ref="app"></App>',
+  components: {
+    App: require('./App')
+  },
+  beforeCreate() {
+    Vue.prototype.$http = Axios(this)
+  }
 })

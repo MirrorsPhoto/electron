df535172f0107984ce750db5dbd91909d4333763
Klimash <andrey.klimash@gmail.com>
2017-11-10 02:54:00 +0300

    add v-animate to Vue instance

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import VueElectron from 'vue-electron'
-import Axios from './plugins/axios'
 import Online from 'online-js'
-import store from './store'
+import Axios from './plugins/axios'
+import Animate from './plugins/v-animate'
+import store from './plugins/store'
 
 const url = 'http://api.jonkofee.ru'
 
 Vue.use(VueElectron)
-Vue.config.productionTip = false
-Vue.config.performance = true
+Vue.use(Animate)
 Vue.prototype.$online = process.env.NODE_ENV !== 'development' && Online({ url })
+
+Vue.config.productionTip = false
 
 new Vue({
   el: '#app',

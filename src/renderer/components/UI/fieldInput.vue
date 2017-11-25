e464f56f68e9f13819978c7d33d047c73436f927
Klimash <andrey.klimash@gmail.com>
2017-11-25 17:53:54 +0300

    new select-component

---
+++ src/renderer/components/UI/fieldInput.vue
@@ -0,0 +1,165 @@
+<template>
+  <div
+    :style="{ width }"
+    @mouseleave="addCloseTimer"
+    @mouseenter="removeCloseTimer"
+    >
+
+    <input
+      ref="input"
+      :value="value"
+      :type="type"
+      :placeholder="placeholder"
+      :disabled="disabled"
+      autocomplete="off"
+      @input="onInput"
+      @focus="$emit('focus')"
+      @blur="$emit('blur')"
+      @keydown="$emit('keydown', $event)"
+      @keydown.escape="$refs.input.blur()"
+    >
+
+    <label
+      v-text="label"
+      :class="{ active: value }"
+    ></label>
+
+    <slot></slot>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: [Number, String],
+      default: ''
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    value: {
+      type: [Number, String],
+      default: ''
+    },
+    numbersOnly: {
+      type: Boolean,
+      default: false
+    },
+    maxLen: {
+      type: [Number, Boolean],
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      timer: null
+    }
+  },
+  methods: {
+    onInput(e) {
+      let { value } = e.target
+      const { maxLen, numbersOnly } = this
+
+      if (maxLen && value.length > maxLen) {
+        value = value.substr(0, maxLen)
+      }
+      if (numbersOnly) {
+        value = value.replace(/[^0-9]/g, '')
+      }
+
+      this.$refs.input.value = value
+      this.$emit('input', value)
+    },
+
+    /**
+     * Придумано СамБэком из Амвросиевки
+     */
+    // Задержка закрытия опций при убирании курсора с поля
+    addCloseTimer() {
+      this.timer = setTimeout(() => this.$refs.input.blur(), 500)
+    },
+    // Отменяем закрытие, если курсор наведен обратно
+    removeCloseTimer() {
+      if (this.timer) {
+        clearTimeout(this.timer)
+        this.timer = null
+      }
+    }
+
+  },
+  mounted() {
+    this.autofocus && this.$refs.input.focus()
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import "../../styles_config.sass"
+
+div
+  display: inline-block
+  height: 40px
+  line-height: 40px
+  position: relative
+
+  & > *
+    cursor: pointer
+
+  & input
+    background: transparent
+    width: 100%
+    height: 40px
+    padding: 17px 0 7px
+    border: none
+    border-bottom: 1px solid $medium
+    outline: none
+    text-overflow: ellipsis
+    transition: all .3s ease
+    position: relative
+    z-index: 1
+
+    &:focus
+      padding-bottom: 6px
+      border-bottom: 2px solid $primary-color
+
+    &::-webkit-outer-spin-button, &::-webkit-inner-spin-button
+      -webkit-appearance: none
+      margin: 0
+
+  & label
+    color: $hard
+    line-height: 16px
+    position: absolute
+    top: 16px
+    left: 0
+    transition: .2s ease-out
+    z-index: 0
+
+  input:focus + label
+    color: $primary-color
+
+  input:focus + label, .active
+    font-size: .8em
+    position: absolute
+    top: -2px
+
+</style>
--- src/renderer/components/auth.vue
+++ src/renderer/components/auth.vue
@@ -2,21 +2,21 @@
   <form :class="{ error: errorMessages.length > 0 }" @submit.prevent="auth()">
 
     <window-buttons></window-buttons>
-    
+
     <logo class="logo"></logo>
-    
-    <field
+
+    <field-input
       placeholder="Логин"
       v-model="user.login"
       autofocus
-    ></field>
-    
-    <field
+    ></field-input>
+
+    <field-input
       placeholder="Пароль"
       type="password"
       v-model="user.password"
-    ></field>
-    
+    ></field-input>
+
     <input type="submit">
 
     <p>
@@ -54,7 +54,7 @@ export default {
           const
             { data }  = await this.$http.post('login', user),
             { token } = data.response
-          
+
           localStorage.setItem('token', token)
           this.$emit('logIn', token)
         } catch(err) {
@@ -66,7 +66,7 @@ export default {
     }
   },
   components: {
-    field: require('./UI/field'),
+    fieldInput: require('./UI/fieldInput'),
     logo: require('./UI/logo'),
     windowButtons: require('./UI/windowButtons')
   }
@@ -76,7 +76,7 @@ export default {
 <style lang="sass" scoped>
 @import '../styles_config.sass'
 
-form 
+form
   background: #fff
   width: 350px
   padding: 50px 50px 70px
@@ -103,21 +103,21 @@ form
     top: 410px
     left: 0
     right: 0
-    
-    & span 
+
+    & span
       color: inherit
 
   &.error
     animation: shake 0.2s ease-in-out 0s 2
-    
-@keyframes shake 
-  0% 
+
+@keyframes shake
+  0%
     margin-left: 0rem
-  25% 
+  25%
     margin-left: 0.5rem
   75%
     margin-left: -0.5rem
-  100% 
+  100%
     margin-left: 0rem
 
 </style>

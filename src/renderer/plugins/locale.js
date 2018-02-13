e1fe83e29eb794918700dd8420d9de75dba1bbf2
Klimash <andrey.klimash@gmail.com>
2018-02-14 00:47:34 +0300

    add variables to $locale method

--- src/renderer/plugins/locale.js
+++ src/renderer/plugins/locale.js
@@ -1,11 +1,19 @@
 const currentLanguage = navigator.language.substr(0, 2)
 const locale = require('../locales/' + currentLanguage)
 
-export default (phrase) => {
-  const result = eval('locale.' + phrase)
+export default (phrase, variables) => {
+  let result = eval('locale.' + phrase)
 
   if (typeof result !== 'string') {
     throw new Error('Invalid phrase: ' + phrase)
+  }
+
+  if (typeof variables === 'object') {
+    for (const key in variables) {
+      const pattern = new RegExp(`%${key}%`, 'g')
+      const value = variables[key]
+      result = result.replace(pattern, value)
+    }
   }
 
   return result

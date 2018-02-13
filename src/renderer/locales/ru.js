ad1e33c4bda01c7737f13ed338b3887e63f08aba
Klimash <andrey.klimash@gmail.com>
2018-02-14 00:13:50 +0300

    add menu and role locale

--- src/renderer/locales/ru.js
+++ src/renderer/locales/ru.js
@@ -10,5 +10,18 @@ module.exports = {
     },
     invalid_login_or_pass: 'Неверный логин или пароль',
     empty_login_or_pass: 'Заполните все поля формы'
+  },
+  user: {
+    roles: {
+      admin: 'Администратор',
+      staff: 'Оператор'
+    }
+  },
+  menu: {
+    dashboard: 'Главный экран',
+    storehouse: 'Товары',
+    statistic: 'Статистика',
+    settings: 'Настройки',
+    logout: 'Выйти'
   }
 }

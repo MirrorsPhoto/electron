ffbe06882e842dd7ed0e3c2bbfc7089d9daddaf1
jonkofee <jonkofee@icloud.com>
2019-02-26 19:57:48 +0300

    :fire: remove modal style and assets

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -92,63 +92,6 @@ h3
 h4
   font-size: 1.2em
 
-.bg
-  background: url(./assets/DSC_6015.jpg) bottom center no-repeat
-  background-size: cover
-  height: 100vh
-  display: flex
-  align-items: center
-  justify-content: center
-
-.release_info
-
-  & #user-content-bug + h3:before, & #user-content-feature + h3:before
-    display: inline-block
-    width: 20px
-    margin-right: 10px
-    vertical-align: text-top
-
-  & #user-content-bug
-
-    & + h3:before
-      content: url('./assets/bug.svg')
-
-    & ~ ul > li:before
-      color: $red
-
-  & #user-content-feature
-
-    & + h3:before
-      content: url('./assets/feature.svg')
-
-    & ~ ul > li:before
-      color: $green
-
-  & ul
-    list-style: none
-    margin-bottom: 5px
-    padding-left: 30px
-
-    & li
-      text-indent: -18px
-
-      &:before
-        content: '•'
-        font-size: 1.3em
-        margin-right: 10px
-
-      & > ul
-        list-style: none
-        padding-left: 0
-
-        & li
-          text-indent: 0
-
-          &:before
-            content: '-'
-            color: #333
-            margin-right: 10px
-
 .scale-enter-active, .scale-leave-active
   transition: all .3s ease
 
